2ffde5aad9fc6d744b73c9828616ff651a5c24f1
chakraborty001 <anikchakraborty998@gmail.com>
2026-08-30 18:25:50 +0600

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Portfolio',
+  description: 'My Personal Portfolio',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
